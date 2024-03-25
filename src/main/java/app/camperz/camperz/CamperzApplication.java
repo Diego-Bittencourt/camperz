1632ef8dcea18e61fbfcc43bcbef8d6ef439ec77
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2024-03-25 23:59:56 +0900

    initial commit

---
+++ src/main/java/app/camperz/camperz/CamperzApplication.java
@@ -0,0 +1,17 @@
+package app.camperz.camperz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class CamperzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamperzApplication.class, args);
+
+
+	}
+
+
+}
---
+++ src/test/java/app/camperz/camperz/CamperzApplicationTests.java
@@ -0,0 +1,13 @@
+package app.camperz.camperz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamperzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
